d3ca4e0eee7e3679fa38b877f27115212a79edc7
m-ghiringhelli <mghiring@gmail.com>
2022-02-17 15:52:37 -0800

    setup test for render function

---
+++ render-utils.js
@@ -0,0 +1,3 @@
+export function renderListItem(item) {
+    return;
+}
--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,39 @@
 import './example.test.js';
+import { renderListItem } from '../render-utils.js';
+const test = QUnit.test;
+
+const test1 = {
+
+};
+
+const test2 = {
+
+};
+
+test('renderListItem should create element for list item', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = `<li class="in-cart">frozen pizza</li>`;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderListItem(test1);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('renderListItem should create element for list item without class', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = `<li>coffee</li>`;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderListItem(test2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
+});
